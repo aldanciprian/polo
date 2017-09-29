a4a8be1562dc17c357f86c8619efd5f60f774d52
aldanciprian <aldanciprian@gmail.com>
2017-09-29 15:25:27 +0300

    added manage

---
+++ manage_poloniex_trade_monoton.sh
@@ -0,0 +1,17 @@
+#!/bin/sh 
+
+while [ 1 ]
+do
+	PID_OUT=`ps -ef | grep poloniex_trade_monoton.pl | grep -v grep`
+	# echo ${PID_OUT}
+	if [ $? -eq 0 ]
+	then
+		#found one
+		PID=`echo ${PID_OUT} | awk '{print $2}'`
+		echo "poloniex_trade_monoton is ${PID}"
+	else
+		./poloniex_trade_monoton.pl 20
+		
+	fi
+	sleep 2s
+done
