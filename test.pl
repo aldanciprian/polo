bcddf69a215106b074e3d9cdd6f073c2533a1ee7
aldanciprian <aldanciprian@gmail.com>
2017-10-08 18:54:24 +0300

    selenium 2

--- test.pl
+++ test.pl
@@ -15,7 +15,8 @@ use File::Basename;
 use DBI;
 use Devel::StackTrace;
 
-use Selenium::Chrome;
+#use Selenium::Chrome;
+use Selenium::Remote::Driver;
 use Selenium::Remote::WebElement;
 use Selenium::Remote::WDKeys;
 #my $url = "https://www.investing.com/currencies/btc-usd-technical";
@@ -23,24 +24,71 @@ use Selenium::Remote::WDKeys;
 
 #print Dumper $content;
 
-my $driver = Selenium::Chrome->new;
+my $driver = new Selenium::Remote::Driver('browser_name'       => 'chrome','platform'           => 'LINUX');
+
+#print Dumper $driver;
+
+#my $driver = new Selenium::Remote::Driver();
 
 $driver->get('https://www.investing.com/currencies/btc-usd-technical');
+
+sleep(2);
 $driver->send_keys_to_active_element(KEYS->{'escape'});
 #print $driver->get_body();
 #print Dumper $driver->get_alert_text;
 
-
 my $element =  $driver->find_element_by_link_text("5 Hours");
 $element->click();
-print $driver->get_body();
+#print $driver->get_body();
  #print $driver->get_page_source();
 
-#element = $driver->find_element_by_id("techinalContent");
+sleep(2);
+
+$element = $driver->find_element_by_id("techinalContent");
+
+my $element_table = $driver->find_child_element($element,"//table[\@class='genTbl closedTbl technicalIndicatorsTbl smallTbl float_lang_base_1']");
+
+print $element_table->get_text();
+
+print "found table\n";
+my $element_trs = $driver->find_child_elements($element_table,"//tbody");
+#print "found tbody\n";
+#print "tbody : \n".$element_tbody->get_text()."\n";
+#my $childs = $driver->find_child_elements($element_tbody,"//tr");
+#print "found trs\n";
+print "size of childs ".@{$element_trs}." \n";
+#foreach (@{$childs})
+#{
+#	print $_->get_text()."\n\n\n";
+#	my $child = $driver->find_child_element($_,"//td[\@class='first left symbol']");
+#	print  $child->get_text()." ";
+#	$child = $driver->find_child_element($_,"//td[\@class='left textNum bold']");
+#	print  $child->get_text()."\n";	
+#	#sleep(1);
+#}
+
+#print $element->get_text();
+#print $element->get_value();
+#print $element->get_tag_name();
+# my $childs = $driver->find_child_elements($element, "//td[\@class='first left symbol']");
+# my $size = @{$childs};
+# print "size is $size \n";
+#print Dumper $childs;
+#print $childs->[1]->get_value();
+#print $childs->[1]->get_text();
+#foreach (@{$childs})
+#{
+#	#print $_->get_value()."\n";
+#	print  $_->get_text()."\n";
+#	#sleep(1);
+#}
 #rint Dumper $element->getElementText();
 #rint $element->get_text();
 #print Dumper $element;
 #$element->click();
-$driver->pause(100000);  # 10 second delay
+$driver->pause(10000);  # 10 second delay
 # when you're done
-$driver->shutdown_binary;
+$driver->close();
+
+$driver->quit();
+
